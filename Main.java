10720a18e3d2d130b0c3b308a3c81233f69c071c
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-07-11 14:10:44 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,105 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		//T = fs.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int n = fs.nextInt();
+			int q = fs.nextInt();
+			long[] a = fs.readLongArray(n);
+			long[] b = fs.readLongArray(n);
+			long[] prefixSum = new long[n];
+			prefixSum[0] = a[0] * b[0];
+			for (int i = 1; i < n; i++) {
+				prefixSum[i] = prefixSum[i - 1] + (a[i] * b[i]);
+			}
+			for (int i = 0; i < q; i++) {
+				int l = fs.nextInt();
+				int r = fs.nextInt();
+				if (l - 2 < 0) {
+					out.println(prefixSum[r - 1]);
+					continue;
+				}
+				out.println(prefixSum[r - 1] - prefixSum[Math.max(0, l - 2)]);
+			}
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+		
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextLong();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
